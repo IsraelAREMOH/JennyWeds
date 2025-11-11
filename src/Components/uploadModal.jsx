34c4e99c5e3a16b1f9451d0eeab7bf0b43fa1940
Israel AREMOH <israelaremoh@gmail.com>
2025-11-11 22:22:58 +0100

    updated text color in upload modal

--- src/Components/uploadModal.jsx
+++ src/Components/uploadModal.jsx
@@ -81,7 +81,7 @@ export default function UploadModal({ isOpen, onClose, onUploadSuccess }) {
           placeholder="Enter your name"
           value={uploader}
           onChange={(e) => setUploader(e.target.value)}
-          className="w-full border border-gray-300 p-2 rounded-lg mb-3 focus:outline-none focus:ring-2 focus:ring-[#6F4E37]"
+          className="w-full border text-gray-500 border-gray-300 p-2 rounded-lg mb-3 focus:outline-none focus:ring-2 focus:ring-[#6F4E37]"
         />
 
         {/* File Input */}
@@ -98,7 +98,7 @@ export default function UploadModal({ isOpen, onClose, onUploadSuccess }) {
             <img
               src={preview}
               alt="Preview"
-              className="w-full h-48 object-cover rounded-lg"
+              className="w-full text-gray-500 h-48 object-cover rounded-lg"
             />
           </div>
         )}
